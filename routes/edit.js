f57b41617601d0c6353816c194eeba6240019f98
Nikiforakis Manos <nikiforakis.m@gmail.com>
2015-11-21 22:46:20 +0200

    *Added CUSTOM profile configuration. Most of the code is common with the WEEKLY configuration code...

--- routes/edit.js
+++ routes/edit.js
@@ -7,7 +7,6 @@ var db = new sqlite3.Database('./sensor-data.sqlite');
 
 function get_sensors(req,res,next){
 	// used by both router.post('/get_timewindow_by_id',...) and router.get('/:id',...)
-	if (req.params.id!=4){
 			db.all("SELECT  id, location FROM sensors;", function(err,rows){
 				if(err){
 					console.error(err);
@@ -17,12 +16,6 @@ function get_sensors(req,res,next){
 				// console.log(req.sensors);
 				next();
 				});
-	}
-	else
-	{
-		next();
-	}
-
 }
 
 
@@ -50,10 +43,10 @@ router.post('/update_temp',function(req, res){
     var sensor_ids = req.body.sensor_ids; //array
     var ids = sensor_ids.join(','); //array->string
 
-    var profile = {2:'CONSTANT_PROFILE',1:'DAY_PROFILE',3:'NIGHT_PROFILE'};//I could get this from a query...index = time_window_id
+    var locked_timewindows = {2:'CONSTANT_PROFILE',1:'DAY_PROFILE',3:'NIGHT_PROFILE'};//I could get this from a query...index = time_window_id
 
-    for (var index in profile) { //forEach is not working for js objects - only arrays!
-    	if (name==profile[index]){
+    for (var index in locked_timewindows) { //forEach is not working for js objects - only arrays!
+    	if (name==locked_timewindows[index]){
     		db.run("UPDATE time_window SET temp=?, sensor_ids=? WHERE id=?",targettemp,ids,index,function(err){
     			if (err==null){
     				res.contentType('json');
@@ -77,41 +70,64 @@ router.post('/update_temp',function(req, res){
 
 function get_weekly_schedule_data(req, res, next){
 	var schedule = [];
-	db.each("SELECT id, daynum FROM schedule WHERE profile_id=3;", function(err,row){
+	var profile_id = {'WEEKLY':3, 'CUSTOM':4};
+	var id = profile_id[req.body.profile];
+	db.each("SELECT id, daynum FROM schedule WHERE profile_id=?;",id, function(err,row){
 			if (err){
 				console.error(err);
 				return next(err);
 			}
-			schedule[row.daynum] = row.id;
+			if (row.daynum=='*'){ //this is needed for the CUSTOM MODE
+				schedule.push(row.id);
+			}
+			else
+			{
+				schedule[row.daynum] = row.id;
+			}
 	},function(){
 		req.schedule = schedule;
+		req.profile_id = id;
 		next();
 	})
 }
 
 router.post('/update_timetable', get_weekly_schedule_data, function(req,res){
 	var newtimewindows = req.body.data; //double array
+	var profile_name = req.body.profile;
 	var schedule = req.schedule;
-	// console.log(schedule);
-	if (schedule!=undefined && schedule!=null){ //propably unneeded check...
+	if (schedule!=undefined && schedule!=null){ //propably unnessesary check...
 		db.serialize(function(){
-			db.run("DELETE FROM timetable WHERE schedule_id IN (SELECT id FROM schedule WHERE schedule.profile_id =3);", function(err){
+			db.run("DELETE FROM timetable WHERE schedule_id IN (SELECT id FROM schedule WHERE schedule.profile_id =?);",req.profile_id, function(err){
 				if (err){
 					console.error(err);
 					return next(err);
 				}
 			});
-			for (var i=0; i<=6; i++){
-				for (var j in newtimewindows[i]){
-					db.run("INSERT INTO timetable (schedule_id, time_window_id) VALUES (?,?);",[schedule[i],newtimewindows[i][j]],function(err){
-						if(err){
-							console.error(err);
-							return next(err);
-							}
-						} //end of callback
-					) //end of db.run
-				} //end of inner for loop
-			} //end of outer for loop
+			if (profile_name=='WEEKLY'){
+				for (var i=0; i<=schedule.length; i++){
+					for (var j in newtimewindows[i]){
+						db.run("INSERT INTO timetable (schedule_id, time_window_id) VALUES (?,?);",[schedule[i],newtimewindows[i][j]],function(err){
+							if(err){
+								console.error(err);
+								return next(err);
+								}
+							} //end of callback
+						) //end of db.run
+					} //end of inner for loop
+				} //end of outer for loop				
+			}
+			else if (profile_name=='CUSTOM'){
+					for (var j in newtimewindows){
+						db.run("INSERT INTO timetable (schedule_id, time_window_id) VALUES (?,?);",[schedule[0],newtimewindows[j]],function(err){
+							if(err){
+								console.error(err);
+								return next(err);
+								}
+							} //end of callback
+						) //end of db.run
+					} //end of inner for loop
+			
+			}
 			//if no error until here it is safe to sent a successfull response!
 			res.contentType('json');
 			res.send({result:'ok'});
@@ -127,7 +143,7 @@ router.post('/update_timetable', get_weekly_schedule_data, function(req,res){
 
 
 function get_timewindows(req, res,next){
-	if (req.params.id==3){
+	if (req.params.id==3 || req.params.id==4){
 		db.all("SELECT * FROM time_window WHERE id NOT IN(1,2,3) ORDER BY time_window.on_time asc", function(err, rows){ //CONSTANT-DAY-NIGHT PROFILE timewindows should not be used in WEEKLY... (or CUSTOM) One can setup other profiles
 			if (err){
 				console.error(err);
@@ -144,8 +160,8 @@ function get_timewindows(req, res,next){
 }
 
 function get_schedule(req, res, next){
-	if (req.params.id==3){
-		db.all("SELECT id, daynum FROM schedule WHERE profile_id=3 ORDER BY daynum ASC;", function(err, rows){
+	if (req.params.id==3){ //required only for the WEEKLY MODE
+		db.all("SELECT id, daynum FROM schedule WHERE profile_id=? ORDER BY daynum ASC;",req.params.id, function(err, rows){
 			if (err){
 				console.error(err);
 				return next(err);
@@ -163,9 +179,10 @@ function get_schedule(req, res, next){
 }
 
 function get_timewindows_perday(req, res, next){
-	if (req.params.id==3){
+	//Used by both Weekly and Custom Profiles to get the corresponding timewindows.
+	if (req.params.id==3 || req.params.id==4){
 		var timewindowsperday=[];
-		db.each("select time_window.*, schedule.daynum from time_window join timetable on time_window.id = timetable.time_window_id join schedule on timetable.schedule_id = schedule.id where schedule.profile_id=3 ORDER BY time_window.on_time ASC;", function(err, row){
+		db.each("select time_window.*, schedule.daynum from time_window join timetable on time_window.id = timetable.time_window_id join schedule on timetable.schedule_id = schedule.id where schedule.profile_id=? ORDER BY time_window.on_time ASC;",req.params.id, function(err, row){
 			if (err){
 				console.error(err);
 				return next(err);
@@ -182,6 +199,7 @@ function get_timewindows_perday(req, res, next){
 		next();
 	}
 }
+
 
 
 router.get('/:id', get_sensors, get_timewindows, get_schedule, get_timewindows_perday, function(req, res) {
@@ -212,8 +230,11 @@ router.get('/:id', get_sensors, get_timewindows, get_schedule, get_timewindows_p
 				res.render('edit/daynight', {dayTemp: req.dayTemp, nightTemp: req.nightTemp, sensors: req.sensors, base_url:base_url});
 	        });
 	 }
-	 else if (req.params.id==3){ //WEEKLY
+	 else if (req.params.id==3){ //WEEKLY MODE
 	 	res.render('edit/weekly', {sensors: req.sensors, schedule: req.schedule, timewindow: req.timewindows, timewindowsperday:req.timewindowsperday, base_url:base_url});
+	 }
+	 else if (req.params.id==4){ //CUSTOM MODE
+	 	res.render('edit/custom', {sensors: req.sensors, timewindow: req.timewindows, timewindowsperday:req.timewindowsperday, base_url:base_url})
 	 }
 })
 
