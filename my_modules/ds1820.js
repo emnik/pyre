566906cb91f742c3ca77d6f896745555f4fc5f32
Nikiforakis Manos <nikiforakis.m@gmail.com>
2015-11-22 23:31:19 +0200

    *Much rework on the core functions of temperature logging (added proper callbacks propagating the errors until I can handle them...) *Database changes mainly in the sensors table *Added new xbee sensor discovery and storage [In the next commit: should change the get_sensor() functions throughout the code to use only the enabled sensors!]

--- my_modules/ds1820.js
+++ my_modules/ds1820.js
@@ -7,52 +7,54 @@
 
   // Setup up database CRUD and temperature data access
 
-
-
   // Write a single temperature record in JSON format to database table.
-  function insertLocalTemp(data){
+  function insertLocalTemp(data, callback){
      //var db = new sqlite3.Database('./sensor-data.sqlite');
      // data is a javascript object
-     var statement = db.prepare("INSERT INTO sensor_data (timestamp, sensor_id, value) VALUES (?, 1, ?)");
-     // Insert values into prepared statement
-     statement.run(data.temperature_record[0].unix_time, data.temperature_record[0].celsius, function(error){
+     db.run("INSERT INTO sensor_data (timestamp, sensor_id, value) VALUES (?, 1, ?)",[data.temperature_record[0].unix_time, data.temperature_record[0].celsius], function(error){
        if (error){
-         console.log("SQL error from ds1820 module: ");
-         console.log(error);
-       }
+         callback(error);
+       }      
      });
-     // Execute the statement
-     statement.finalize();
-     //db.close();
   }
 
 
   // Read current temperature from local sensor
 
-  function readLocalTemp(callback){
+  function readLocalTemp(insertfunc, callback){
     ds18b20.temperature('28-000006b3513e', function(err, value) {
      if (err){
-         console.error(err);
-         process.exit(1);
+         return callback(err);
       }
     var data = {
       temperature_record:[{
-      unix_time: Date.now(),
-      celsius: Math.round(value * 10) / 10 //round temperature to 1 decimal digit
-      }]};
+        unix_time: Date.now(),
+        celsius: Math.round(value * 10) / 10 //round temperature to 1 decimal digit
+        }]
+      };
 
       // Execute call back with data
-      callback(data);
+      insertfunc(data, function(err){
+        if(err){
+          callback(err);
+        }
+      });
     });
   };
 
 
   // Create a wrapper function which we'll use specifically for logging
-  function logLocalTemp(interval){
+  function logLocalTemp(interval, callback){
         // Call the readTemp function with the insertLocalTemp function as output to get initial reading
-        readLocalTemp(insertLocalTemp);
+        readLocalTemp(insertLocalTemp, function(error){
+          return callback(error);
+        });
         // Set the repeat interval (milliseconds). Third argument is passed as callback function to first (i.e. readTemp(insertLocalTemp)).
-        setInterval(readLocalTemp, interval, insertLocalTemp);
+        setInterval(readLocalTemp, interval, insertLocalTemp, function(error){
+          if(error){
+            callback(error);
+          }
+        });
   };
 
 
--- my_modules/ds1820_xbee.js
+++ my_modules/ds1820_xbee.js
@@ -4,7 +4,7 @@ var util = require('util');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./sensor-data.sqlite');
 
-function readRemoteTemp(callback)
+function readRemoteTemp(insertfunc, callback)
 {
   var xbeeAPI = new xbee_api.XBeeAPI({
     api_mode: 1, //ATAP1 -without escape characters
@@ -20,45 +20,62 @@ function readRemoteTemp(callback)
 
   xbeeAPI.on("frame_object", function(frame) {
   	console.log(frame);
-    if (frame.remote16 === '0002') // this is the address of the remote Xbee
-    {
-      var buf = frame.data;
-      var value = buf.readFloatLE(0);
-      console.log(value);
-      var data = {
-        temperature_record:[{
-        unix_time: Date.now(),
-        celsius: Math.round(value * 10) / 10 //round temperature to 1 decimal digit
-        }]};
-      // console.log(data);
-      if (data.temperature_record[0].celsius!=""){
-        callback(data);
-      }
+    var xbee_id = frame.remote16;
+    var buf = frame.data;
+    var value = buf.readFloatLE(0);
+    // console.log(value);
+    var data = {
+      temperature_record:[{
+      unix_time: Date.now(),
+      celsius: Math.round(value * 10) / 10, //round temperature to 1 decimal digit
+      xbee_id: xbee_id
+      }]};
+    // console.log(data);
+    if (data.temperature_record[0].celsius!=""){
+      insertfunc(data, function(error){
+        if (error){
+          callback(error);
+        }
+      });
     }
   })
 }
 
 
   // Write a single temperature record in JSON format to database table.
-  function insertRemoteTemp(data){
-     //var db = new sqlite3.Database('./sensor-data.sqlite');
-     // data is a javascript object
-     var statement = db.prepare("INSERT INTO sensor_data (timestamp, sensor_id, value) VALUES (?, 2, ?)");
-     // Insert values into prepared statement
-     statement.run(data.temperature_record[0].unix_time, data.temperature_record[0].celsius, function(error){
-       if (error){
-         console.log("SQL error from xbee module: ");
-         console.log(error);
-       }
-     });
-     // Execute the statement
-     statement.finalize();
-     //db.close();
+  function insertRemoteTemp(data, callback){
+    db.all('SELECT id FROM sensors WHERE xbee_id=? LIMIT 1', data.temperature_record[0].xbee_id, function(err,row){
+      if(err){
+        return callback(err);
+      }
+      if(row.length==0){ //New remote sensor! Just Insert it in the sensor table and mark it's status as disabled. The user has first to set location and enable
+        db.run("INSERT INTO sensors (type, xbee_id, status, protected) VALUES ('TEMPERATURE', ?, 0, 0)",data.temperature_record[0].xbee_id, function(err){
+         if (err){
+           callback(err);
+         }
+         console.log("We have a new temp sensor online with xbee id: "+data.temperature_record[0].xbee_id+" which is now registered (but disabled) with id: "+this.lastID);
+       });
+      }
+      else //existing sensor
+      {
+        var sensor_id=row[0].id;
+        db.run("INSERT INTO sensor_data (timestamp, sensor_id, value) VALUES (?, ?, ?)",[data.temperature_record[0].unix_time, sensor_id, data.temperature_record[0].celsius], function(err){
+         if (err){
+           callback(err);
+         } 
+       })         
+      }
+    })
   }
 
 
-  function logRemoteTemp(){
-    readRemoteTemp(insertRemoteTemp);
+  function logRemoteTemp(callback){
+    readRemoteTemp(insertRemoteTemp, function(err){
+      if(err){
+        // console.log(err);
+        callback(err);
+      }
+    });
   }
 
 module.exports.logRemoteTemp = logRemoteTemp;
